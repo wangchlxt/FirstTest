5b029d1b956ab6020d32c33f0d14c4989dd274b8
Arvin <wangchenglonge@qq.com>
2017-09-08 16:45:29 +0800

    添加中文拼音处理

--- CSharpUtils/FileUtil.cs
+++ CSharpUtils/FileUtil.cs
@@ -127,5 +127,97 @@ namespace CSharpUtils
             StreamWriter.Close();
         }
         #endregion
+
+        /// <summary>
+        /// 测试向从二进制文件中读取数据，并显示到终端上.
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <returns>文件内容</returns>
+        public static string ReadBinaryFile(string fileName)
+        {
+            string message = string.Empty;
+            // Console.WriteLine("读取二进制文件信息开始。");
+            FileStream fs = null;
+            BinaryReader br = null;
+            try
+            {
+                // 首先判断，文件是否已经存在
+                if (!File.Exists(fileName))
+                {
+                    // 如果文件不存在，那么提示无法读取！
+                    // Console.WriteLine("二进制文件{0}不存在！", fileName);
+                    return string.Empty;
+                }
+
+
+                fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                br = new BinaryReader(fs);
+
+                // int a = br.ReadInt32();
+                // double b = br.ReadDouble();
+                // int c = br.ReadInt32();
+                // byte len = br.ReadByte();
+                // char[] d = br.ReadChars(len);
+
+                // Console.WriteLine("第一个数据:{0}", a);
+                // Console.WriteLine("第二个数据:{0}", b);
+                // Console.WriteLine("第三个数据:{0}", c);
+                // Console.WriteLine("第四个数据 (长度为{0}):", len);
+                // foreach (char ch in d)
+                // {
+                //    Console.Write(ch);
+                // }
+                // Console.WriteLine();
+                //完整的读取文件类容需要获取文件的长度
+                int count = (int)fs.Length;
+                byte[] buffer = new byte[count];
+                br.Read(buffer, 0, buffer.Length);
+                message = Encoding.Default.GetString(buffer);
+                // message = br.ReadString();
+
+                // 读取完毕，关闭.
+                br.Close();
+                fs.Close();
+
+                br = null;
+                fs = null;
+            }
+            catch (Exception ex)
+            {
+                // Console.WriteLine("在读取文件的过程中，发生了异常！");
+                // Console.WriteLine(ex.Message);
+                // Console.WriteLine(ex.StackTrace);
+                throw ex;
+            }
+            finally
+            {
+                if (br != null)
+                {
+                    try
+                    {
+                        br.Close();
+                    }
+                    catch
+                    {
+                        // 最后关闭文件，无视 关闭是否会发生错误了.
+                    }
+                }
+
+                if (fs != null)
+                {
+                    try
+                    {
+                        fs.Close();
+                    }
+                    catch
+                    {
+                        // 最后关闭文件，无视 关闭是否会发生错误了.
+                    }
+                }
+            }
+            // Console.WriteLine("读取二进制文件信息结束。");
+            return message;
+        }
+
     }
 }
--- CSharpUtils/SecretUtil.cs
+++ CSharpUtils/SecretUtil.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web.Security;
+using System.Xml;
 
 namespace CSharpUtils
 {
@@ -247,5 +248,124 @@ namespace CSharpUtils
             cs.FlushFinalBlock();
             return Encoding.Default.GetString(ms.ToArray());
         }
+
+        #region public static string SqlSafe(string value) 检查参数的安全性
+        /// <summary>
+        /// 检查参数的安全性
+        /// </summary>
+        /// <param name="value">参数</param>
+        /// <returns>安全的参数</returns>
+        public static string SqlSafe(string value)
+        {
+            value = value.Replace("'", "''");
+            // value = value.Replace("%", "'%");
+            return value;
+        }
+        #endregion
+        
+
+        /// <summary>
+        /// 获取私钥
+        /// </summary>
+        /// <param name="digitalSignatureFile">数字证书文件</param>
+        /// <param name="signedPassword">签名密码</param>
+        /// <returns>私钥</returns>
+        public static string GetPrivateKey(string digitalSignatureFile, string signedPassword)
+        {
+            // 0:这里需要处理异常信息
+            // 1:定义私钥
+            string privateKey = string.Empty;
+            // 2:读取证书文件
+            string digitalSignature = FileUtil.ReadBinaryFile(digitalSignatureFile);
+            // 3:解密文件
+            string xmlFile = SecretUtil.Decrypt(digitalSignature);
+            // 4:按XML文件读取
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xmlFile);
+            string keySignedPassword = xmlDocument.SelectSingleNode("//DigitalSignature/Key").Attributes["SignedPassword"].Value;
+            // 5:若签名密码不对，不应该能读取私钥
+            if (md5(signedPassword, 32).Equals(keySignedPassword))
+            {
+                privateKey = xmlDocument.SelectSingleNode("//DigitalSignature/Key").Attributes["PrivateKey"].Value;
+            }
+            return privateKey;
+        }
+
+        /// <summary>
+        /// 获取签名密码
+        /// </summary>
+        /// <param name="digitalSignatureFile">数字证书文件</param>
+        /// <returns>私钥</returns>
+        public static string GetSignedPassword(string digitalSignatureFile)
+        {
+            // 0:这里需要处理异常信息
+            // 1:定义私钥
+            string signedPassword = string.Empty;
+            // 2:读取证书文件
+            string digitalSignature = FileUtil.ReadBinaryFile(digitalSignatureFile);
+            // 3:解密文件
+            string xmlFile = SecretUtil.Decrypt(digitalSignature);
+            // 4:按XML文件读取
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xmlFile);
+            signedPassword = xmlDocument.SelectSingleNode("//DigitalSignature/Key").Attributes["SignedPassword"].Value;
+            return signedPassword;
+        }
+
+        /// <summary>
+        /// 对数据进行签名
+        /// 
+        /// 将来需要改进为，对散列值进行签名
+        /// </summary>
+        /// <param name="dataToSign">需要签名的数据</param>
+        /// <param name="privateKey">私钥</param>
+        /// <returns>签名结果</returns>
+        public static string HashAndSign(string dataToSign, string privateKey)
+        {
+            ASCIIEncoding byteConverter = new ASCIIEncoding();
+            byte[] DataToSign = byteConverter.GetBytes(dataToSign);
+            try
+            {
+                RSACryptoServiceProvider cryptoServiceProvider = new RSACryptoServiceProvider();
+                cryptoServiceProvider.ImportCspBlob(Convert.FromBase64String(privateKey));
+                byte[] SignedData = cryptoServiceProvider.SignData(DataToSign, new SHA1CryptoServiceProvider());
+                string signedData = Convert.ToBase64String(SignedData);
+                return signedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 验证数字签名
+        /// 
+        /// 将来需要改进为，按散列值进行验证
+        /// </summary>
+        /// <param name="dataToVerify">需要验证的数据</param>
+        /// <param name="signedData">签名结果</param>
+        /// <param name="publicKey">公钥</param>
+        /// <returns>正确</returns>
+        public static bool VerifySignedHash(string dataToVerify, string signedData, string publicKey)
+        {
+            byte[] SignedData = Convert.FromBase64String(signedData);
+
+            ASCIIEncoding byteConverter = new ASCIIEncoding();
+            byte[] DataToVerify = byteConverter.GetBytes(dataToVerify);
+            try
+            {
+                RSACryptoServiceProvider cryptoServiceProvider = new RSACryptoServiceProvider();
+                cryptoServiceProvider.ImportCspBlob(Convert.FromBase64String(publicKey));
+                return cryptoServiceProvider.VerifyData(DataToVerify, new SHA1CryptoServiceProvider(), SignedData);
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
     }
 }
