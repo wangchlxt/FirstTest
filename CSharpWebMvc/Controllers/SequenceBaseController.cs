5c2672bc3a1211cb46b1d8924c3aef1c74983d14
Arvin <wangchenglonge@qq.com>
2017-09-22 11:42:32 +0800

    Sequence
    Action Filter 和 BaseControl: get param 执行顺序

---
+++ CSharpWebMvc/Controllers/SequenceBaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CSharpUtils;
+
+namespace CSharpWebMvc.Controllers
+{
+    public class SequenceBaseController : Controller
+    {
+        public string code
+        {
+            get
+            {
+                string strCode = StringUtil.GenerateRandomStr();
+                LogHelper.Info(String.Format("SequenceBaseController get code：{0}", strCode));
+                return strCode;
+            }
+        }
+    }
+}
---
+++ CSharpWebMvc/Controllers/SequenceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CSharpWebMvc.Filter;
+
+namespace CSharpWebMvc.Controllers
+{
+    public class SequenceController : SequenceBaseController
+    {
+        [MyActionFilter]
+        // GET: Sequence
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [MyActionFilter]
+        public ActionResult GetCode()
+        {
+            ViewBag.code = code;
+            return View();
+        }
+    }
+}
