e75f143d4f799f678ee78b05e7156dedf4d3e07c
Arvin <wangchenglonge@qq.com>
2017-09-28 09:33:04 +0800

    MVC Request

--- CSharpWebMvc/Controllers/RequestController.cs
+++ CSharpWebMvc/Controllers/RequestController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,47 @@ namespace CSharpWebMvc.Controllers
         public ActionResult Index()
         {
             ViewBag.data = Request.Params.ToString();
+            return View();
+        }
+
+        public ActionResult Param()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult DoParamByGet()
+        {
+            Dictionary<string, string> param = new Dictionary<string, string>();
+
+            NameValueCollection nv = Request.QueryString;
+
+            foreach (string key in nv.AllKeys)
+            {
+                param.Add(key, nv[key]);
+            }
+
+            param.Remove("sex");
+
+            ViewBag.param = param;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DoParamByPost()
+        {
+            Dictionary<string, string> param = new Dictionary<string, string>();
+
+            NameValueCollection nv = Request.Form;
+
+            foreach (string key in nv.AllKeys)
+            {
+                param.Add(key, nv[key]);
+            }
+
+            param.Remove("sex");
+
+            ViewBag.param = param;
             return View();
         }
     }
