02ea320961de914a822381a9cc46d11f0d6dc35e
Arvin <wangchenglonge@qq.com>
2017-10-10 17:18:25 +0800

    合并支付码

---
+++ CSharpWebMvc/Controllers/PayQRCodeController.cs
@@ -0,0 +1,46 @@
+﻿using CSharpUtils;
+using CSharpWebMvc.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSharpWebMvc.Controllers
+{
+    public class PayQRCodeController : Controller
+    {
+        // GET: PayQRCode
+        public ActionResult Index()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> pair in Request.QueryString)
+            {
+                sb.Append(pair.Key);
+                sb.Append("=");
+                sb.Append(pair.Value);
+                sb.Append("&");
+            }
+
+            LogHelper.Info(String.Format("Pay ->\r\n url:{0}\r\n UserAgent:{1}\r\n Param:{2}",
+                Request.Url,
+                Request.UserAgent,
+                sb.ToString()));
+            
+            if (Request.UserAgent.IndexOf("AlipayClient") > 0)
+            {
+                LogHelper.Info("支付宝支付");
+                return Redirect("HTTPS://QR.ALIPAY.COM/FKX00658OYYACI96HEXGC8");
+            }
+            else if (Request.UserAgent.IndexOf("MicroMessenger") > 0)
+            {
+                LogHelper.Info("微信支付");
+                ViewBag.type = "wechat";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ CSharpWebMvc/Controllers/ReturnController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSharpWebMvc.Controllers
+{
+    public class ReturnController : Controller
+    {
+        // GET: Return
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ToBaidu()
+        {
+            return Redirect("http://www.baidu.com");
+        }
+
+        public ActionResult ToWechatPay()
+        {
+            return Redirect("");
+        }
+
+        public ActionResult ReturnImage()
+        {
+            return File("..\\Content\\image\\20170912094052172CastlePointLH_ZH-CN13074557115_1920x1080.jpg", "image/JPEG");
+        }
+    }
+}
